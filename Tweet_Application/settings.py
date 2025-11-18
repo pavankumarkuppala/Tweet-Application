d62c68cab5a3319ac0c137d326b6daa15d580cda
Pavan Kumar <kuppalapavankumarroyal@gmail.com>
2025-11-18 15:05:03 +0530

    Update settings.py

--- Tweet_Application/settings.py
+++ Tweet_Application/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +132,9 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -141,6 +146,7 @@ LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT = '/tweet/'
 
 LOGOUT_REDIRECT = '/tweet/'
+
 
 
 
