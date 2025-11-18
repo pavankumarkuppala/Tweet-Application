b0a5566186aae9d429907d65964d3095a077c8d6
Pavan Kumar <kuppalapavankumarroyal@gmail.com>
2025-11-18 13:53:32 +0530

    Update urls.py
    Updated main urlpatterns to route from '/tweet' to '/'

--- Tweet_Application/urls.py
+++ Tweet_Application/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls')),
+    path('', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
